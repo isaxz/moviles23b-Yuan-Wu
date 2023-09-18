84384e9fb590c0d9c79d6e6be805c0515c798c15
isaxz <jajadragon246@gmail.com>
2023-09-17 23:40:57 -0600

    Another proyect removed

--- tarea01/fraction_and_arithmetic_expression/README.md
+++
@@ -1,3 +0,0 @@
-# fraction_and_arithmetic_expression
-
-A new Flutter project.
--- tarea01/fraction_and_arithmetic_expression/android/app/src/main/kotlin/com/example/fraction_and_arithmetic_expression/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fraction_and_arithmetic_expression
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
